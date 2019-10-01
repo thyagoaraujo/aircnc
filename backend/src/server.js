eec63212ad0c06f7db48c4ff8dbb3193cb79badc
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-01 15:35:38 -0300

    config mongodb, mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://omnistack:omnistack@cluster0-d8xpb.mongodb.net/aircnc?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
 
 app.use(express.json());
 app.use(routes);
