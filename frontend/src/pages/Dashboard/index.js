ed34b37313cc66a280557ff53cc807293de289a2
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-03 15:18:52 -0300

    add url thumbnail backend

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
 export default function Dashboard() {
+  const [spots, setSpots] = useState([]);
+
   useEffect(() => {
     async function loadSpots() {
       const user_id = localStorage.getItem('user');
@@ -9,11 +11,23 @@ export default function Dashboard() {
         headers: { user_id }
       });
 
-      console.log(response.data);
+      setSpots(response.data);
     }
 
     loadSpots();
   }, []);
 
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <ul className="spot-list">
+        {spots.map(spot => (
+          <li key={spot._id}>
+            <header />
+            <strong>{spot.company}</strong>
+            <span>{spot.price}</span>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
