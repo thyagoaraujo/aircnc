1d4005933136a353b3362a5bc41c9a04f38f8164
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-03 16:49:31 -0300

    connect api with form new spot, finished

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useMemo } from 'react';
+import api from '../../services/api';
 import camera from '../../assets/camera.svg';
 import './styles.css';
 
-export default function New() {
+export default function New({ history }) {
   const [thumbnail, setThumbnail] = useState(null);
   const [company, setCompany] = useState('');
   const [techs, setTechs] = useState('');
@@ -12,7 +13,23 @@ export default function New() {
     return thumbnail ? URL.createObjectURL(thumbnail) : null;
   }, [thumbnail]);
 
-  function handleSubmit() {}
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const data = new FormData();
+    const user_id = localStorage.getItem('user');
+
+    data.append('thumbnail', thumbnail);
+    data.append('company', company);
+    data.append('techs', techs);
+    data.append('price', price);
+
+    await api.post('/spots', data, {
+      headers: { user_id }
+    });
+
+    history.push('/dashboard');
+  }
 
   return (
     <form onSubmit={handleSubmit}>
