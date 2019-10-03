a7e6ab41d92532f2fbf729bb9c7524d1c1d0c6ab
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-03 16:34:27 -0300

    config preview thumbnail and styled

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import camera from '../../assets/camera.svg';
 import './styles.css';
 
 export default function New() {
+  const [thumbnail, setThumbnail] = useState(null);
   const [company, setCompany] = useState('');
   const [techs, setTechs] = useState('');
   const [price, setPrice] = useState('');
+
+  const preview = useMemo(() => {
+    return thumbnail ? URL.createObjectURL(thumbnail) : null;
+  }, [thumbnail]);
 
   function handleSubmit() {}
 
   return (
     <form onSubmit={handleSubmit}>
-      <label id="thumbnail">
-        <input type="file" />
+      <label
+        id="thumbnail"
+        style={{ backgroundImage: `url(${preview})` }}
+        className={thumbnail ? 'has-thumbnail' : ''}
+      >
+        <input
+          type="file"
+          onChange={event => setThumbnail(event.target.files[0])}
+        />
         <img src={camera} alt="Select img" />
       </label>
 
