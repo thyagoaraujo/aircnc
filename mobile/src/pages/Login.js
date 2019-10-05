37fe1657d0c61cb0dcb6d3a776b834a900de394a
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-05 14:00:06 -0300

    add logo

--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,6 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Login() {
-  return <View />;
+  return (
+    <View>
+      <Image source={logo} />
+    </View>
+  );
 }
