fc1339da44a13f95c15d6bcbc1d600709e5df202
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-08 16:28:44 -0300

    add logo

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Text, AsyncStorage } from 'react-native';
+import {
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  Image,
+  AsyncStorage
+} from 'react-native';
+import logo from '../assets/logo.png';
 
 export default function List() {
   const [techs, setTechs] = useState([]);
@@ -12,5 +19,22 @@ export default function List() {
     });
   }, []);
 
-  return <Text>{techs}</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 10
+  }
+});
