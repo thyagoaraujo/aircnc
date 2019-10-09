7430770f962a5c3b32e939ec29dfbc17541d7c15
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-09 20:27:05 -0300

    config socket.io and http

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@cluster0-d8xpb.mongodb.net/aircnc?retryWrites=true&w=majority',
@@ -20,4 +24,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
