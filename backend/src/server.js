343fd6c6a436d63f27bfce857b2d4aef9d94a897
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-10-09 20:55:02 -0300

    connect socketio backend/frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,12 @@ const app = express();
 const server = http.Server(app);
 const io = socketio(server);
 
+io.on('connection', socket => {
+  console.log('Usuário conectado', socket.id);
+
+  socket.emit('hello', 'World');
+});
+
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@cluster0-d8xpb.mongodb.net/aircnc?retryWrites=true&w=majority',
   {
